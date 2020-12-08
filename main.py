8fc33026eeb440e8b8b04b8e5cb23a1b8d6c224f
Thanaboon Sirirungnapawilai <thanaboon.sirirungnapawilai@gmail.com>
2020-12-08 15:58:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,27 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/table')
+def table():
+    lst = []
+    ref = db .child('event').get()
+    for i in ref.each():
+        commpant = i.val()['compant']
+        email = i.val()['email']
+        fname = i.val()['fname']
+        message = i.val()['message']
+        product = i.val()['product']
+        tel = i.val()['tel']
+        key = i.key()
+        box = {'conpant':commpant, 'email':email, 'fname':fname, 'message': message,
+                'product':product, 'tel':tel, 'key':key}
+        lst.append(box)
+    data = {
+        'ref':lst
+        }
+    return render_template('index.html', data=data)
+
+    
 
 
 if __name__=='__main__':
