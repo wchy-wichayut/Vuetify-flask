89de18fb5a78bbc3eb976a0b13c9c0c8d6651f9a
Thanaboon Sirirungnapawilai <thanaboon.sirirungnapawilai@gmail.com>
2020-12-15 10:53:41 +0700

    h

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, jsonify, request, redirect, url_for
-import json
+from flask.helpers import make_response
 import pyrebase
-from random import randrange
+import json
+from oopclass import FirebaseAPI
 
-with open("config.json", encoding="utf 8")as json_file:
+with open("config.json", encoding='utf 8') as json_file:
     data = json.load(json_file)
     config = data["firebase"]
     firebase = pyrebase.initialize_app(config)
@@ -13,17 +14,71 @@ with open("config.json", encoding="utf 8")as json_file:
 app = Flask(__name__)
 
 
-@app.route("/")
-@app.route("/index")
+@app.route('/')
+@app.route('/index')
 def index():
-
+    
     return render_template('index.html')
 
+@app.route("/home")
+def home():
+    lst = []
+    count = 1
+    ref = db.child('chatbot_transactions').get()
+    for i in ref.each()[62:]:
+        r = i.val()
+        day = i.val()['day']
+        hour = i.val()['hour']
+        img = i.val()['img']
+        message = i.val()['message']
+        mins = i.val()['min']
+        month = i.val()['month']
+        profile = i.val()['profile']
+        reply = i.val()['reply']
+        sec = i.val()['sec']
+        userid = i.val()['userid']
+        year = i.val()['year']
+        key = i.key()
+        box = {'date/time': f'{year}/{month}/{day} {hour}:{mins}:{sec}', 'img':img, 'message':message,
+        'profile':profile, 'reply':reply,'userid':userid, "key":key, "index":count}
+        lst.append(box)
+        count += 1
+    show = {
+        'ref':lst      
+        }
+    return jsonify(show)
+ 
+@app.route('/json_index')
+def update():
+    fb = FirebaseAPI(db, "chatbot_transactions")
+    lst = fb.transaction_index()
+    data = {
+        "ref": lst[::-1]
+    }
+    return jsonify(data)
 
-# @app.route("/welcome")
-# def index():
-#     return render_template('main/welcome.html')
 
 
-if __name__ == "__main__":
+@app.route('/update_index/<id>',methods=["POST"])
+def update_index(id):
+    post_data = request.get_json()
+    print(id)
+    print(post_data)
+    db.child('chatbot_transactions').child(id).update(post_data)
+    return make_response(post_data)
+
+@app.route('/delete_index/<id>',methods=["POST"])
+def delete_index(id):
+    post_data = request.get_json()
+    print(id)
+    print(post_data)
+    db.child("chatbot_transactions").child(id).remove()
+    return make_response(post_data)
+
+@app.route('/demo')
+def demo():
+    return render_template('demo.html')
+
+if __name__ == '__main__':
     app.run(debug=True, port=8080)
+ 
